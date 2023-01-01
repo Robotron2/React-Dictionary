5fee66235a6e4e91950544a363565fd3b02c8cb9
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-01 22:56:01 +0100

    App basic structure set

---
+++ src/Pages/Pages.js
@@ -0,0 +1,9 @@
+const Pages = () => {
+	return (
+		<div>
+			<h4>Individual Pages</h4>
+		</div>
+	)
+}
+
+export default Pages
---
+++ src/Pages/SearchHistory.js
@@ -0,0 +1,9 @@
+const SearchHistory = () => {
+	return (
+		<div>
+			<h3>Search History</h3>
+		</div>
+	)
+}
+
+export default SearchHistory
