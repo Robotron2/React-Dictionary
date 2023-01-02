f20210e3916960874b48d5138bb1d4ed1377eceb
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-02 14:37:51 +0100

    API works perfectly now

--- src/Pages/SearchResults.js
+++ src/Pages/SearchResults.js
@@ -1,7 +1,28 @@
-// import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 const Results = () => {
-	// const [results, setResults] = useState([])
+	const [results, setResults] = useState([])
+	let params = useParams()
+	// let searchUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${params.searchWord}`
+
+	const fetchData = async (word) => {
+		await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+			.then((res) => {
+				console.log(res)
+				return res.json()
+			})
+			.then((data) => {
+				console.log(data) // [{}]
+				setResults(data[0])
+			})
+	}
+
+	useEffect(() => {
+		fetchData(params.searchWord)
+	}, [params.searchWord])
+
+	//console.log(params.searchWord) // Dynamic value
 
 	return (
 		<div>
