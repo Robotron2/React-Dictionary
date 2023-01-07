9a71e7c2e13abb70ce837a9d864685e25c9a7af7
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-07 19:30:24 +0100

    Local storage items displayed without styles on the search history page

--- src/Pages/SearchHistory.js
+++ src/Pages/SearchHistory.js
@@ -1,7 +1,17 @@
 const SearchHistory = () => {
+	const histories = JSON.parse(localStorage.getItem("history"))
+	console.log(histories)
+
 	return (
 		<div>
 			<h3>Search History</h3>
+			{histories.map((history, index) => {
+				return (
+					<div key={index + 1}>
+						<h3>{history}</h3>
+					</div>
+				)
+			})}
 		</div>
 	)
 }
