a65b16b2dbb9a547ec8a76252af7bcbb1a9e1e71
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-10 18:18:27 +0100

    About page styled

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,14 @@
 const About = () => {
 	return (
 		<div>
-			<div className="flow-text">
-				<p>
+			<div className="flow-text" style={{ padding: "2vmin" }}>
+				<div style={{ textAlign: "center", padding: "1rem" }}>
+					<h4 className="about-header" style={{ color: "black" }}>
+						Robotron's Dictionary <span style={{ fontSize: "5vmin", color: "red", padding: "2px" }}>.</span>
+					</h4>
+				</div>
+
+				<p className="about-paragraph" style={{ color: "grey", fontSize: "3vmin" }}>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure commodi molestiae pariatur ullam amet voluptatibus! Et esse, voluptatum enim error dolorem itaque recusandae
 					deleniti eius vel, optio nihil aliquam! Aut facere ratione expedita sequi earum corporis fugiat eveniet quam, recusandae neque, nam nesciunt vitae quia possimus eos repellat
 					maxime. Placeat atque rem at nihil veniam facere, accusantium ut culpa! Id, in maxime officiis, quos sapiente illum numquam excepturi at illo est facere sint quibusdam mollitia
