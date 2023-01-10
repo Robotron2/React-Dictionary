d6f4d41d266e1364acf16fd4288a3ca40eb71ccb
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-10 18:37:16 +0100

    Animate Presence

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import { BrowserRouter } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header"
 import Pages from "./Pages/Pages"
-import { motion } from "framer-motion"
 
 function App() {
 	return (
-		<motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+		<div>
 			<BrowserRouter>
 				<Header />
 				<Pages />
 			</BrowserRouter>
-		</motion.div>
+		</div>
 	)
 }
 
--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -1,18 +1,23 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import About from "./About"
 import Home from "./Home"
 import SearchHistory from "./SearchHistory"
 import Results from "./SearchResults"
+import { AnimatePresence } from "framer-motion"
 
 const Pages = () => {
+	const location = useLocation()
+
 	return (
 		<div className="container">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/search/:searchWord" element={<Results />} />
-				<Route path="/history" element={<SearchHistory />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
+			<AnimatePresence mode="wait" location={location} key={location.pathname}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/search/:searchWord" element={<Results />} />
+					<Route path="/history" element={<SearchHistory />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</AnimatePresence>
 		</div>
 	)
 }
