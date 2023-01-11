8cd80514c11b3033a4f0e0e512e463d132b709c9
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-11 18:57:42 +0100

    Random words prevented from saving to LS

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -17,6 +17,12 @@ const About = () => {
 						maxime. Placeat atque rem at nihil veniam facere, accusantium ut culpa! Id, in maxime officiis, quos sapiente illum numquam excepturi at illo est facere sint quibusdam mollitia
 						ducimus magnam repellendus veniam voluptatem vel. Optio inventore, sunt quae modi ea quam id?
 					</p>
+					<p className="about-paragraph" style={{ color: "grey", fontSize: "3vmin" }}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure commodi molestiae pariatur ullam amet voluptatibus! Et esse, voluptatum enim error dolorem itaque recusandae
+						deleniti eius vel, optio nihil aliquam! Aut facere ratione expedita sequi earum corporis fugiat eveniet quam, recusandae neque, nam nesciunt vitae quia possimus eos repellat
+						maxime. Placeat atque rem at nihil veniam facere, accusantium ut culpa! Id, in maxime officiis, quos sapiente illum numquam excepturi at illo est facere sint quibusdam mollitia
+						ducimus magnam repellendus veniam voluptatem vel. Optio inventore, sunt quae modi ea quam id?
+					</p>
 				</div>
 			</motion.div>
 		</div>
