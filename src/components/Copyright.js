994ca26b4d66c408a107513dac581ec1da4878ba
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-11 19:08:09 +0100

    Copyright component styled

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,5 +1,17 @@
 const Copyright = () => {
-	return <div>Date</div>
+	let currentYear = new Date().getFullYear()
+
+	return (
+		<div>
+			<center>
+				<div className="copyright">
+					<p className="flow-text" style={{ color: "grey", fontSize: "1.2rem" }}>
+						© Robotron {currentYear}
+					</p>
+				</div>
+			</center>
+		</div>
+	)
 }
 
 export default Copyright
